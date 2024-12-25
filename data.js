c8876bc99a43d3da7c290b10f2fce26df4c8da39
zakaria <bouhdjourzakaria@gmail.com>
2024-12-25 22:15:55 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,15 @@
+var todos=[
+    {
+        "title":"reading" ,
+        "desc":"read a book",
+        "duration":"2h",
+        'complete':false
+    },
+    {
+        "title":"watch tuto" ,
+        "desc":"watch tutorial about javascript",
+        "duration":"1h",
+        'complete':false
+    }
+
+];
